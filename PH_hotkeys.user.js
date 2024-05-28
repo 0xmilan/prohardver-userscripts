e9a0052ea2f61555ab0e5b383bc5158739db11e4
Milan Magyar <mm@mac.local>
2024-05-28 03:38:24 +0200

    removed shortcuts related to inserting links (TinyMCE built-in feature)

--- PH_hotkeys.user.js
+++ PH_hotkeys.user.js
@@ -6,7 +6,7 @@
 // @match        https://mobilarena.hu/tema/*
 // @match        https://itcafe.hu/tema/*
 // @match        https://gamepod.hu/tema/*
-// @version      1.0
+// @version      1.1
 // @author       0xmilan
 // @grant        none
 // @updateURL    https://raw.githubusercontent.com/0xmilan/prohardver-userscripts/main/PH_hotkeys.user.js
@@ -23,12 +23,6 @@
             document.querySelector('a[title="Új hozzászólás"]').click();
         };
 
-        // insert link
-        if ((event.ctrlKey || event.metaKey) && event.key === 'k') {
-            event.preventDefault();
-            document.querySelector('button[data-tmce-toggle-special="link"]').click();
-        };
-
         // toggle off topic text
         if ((event.ctrlKey || event.metaKey) && event.key === 'h') { // h for "halvany"
             event.preventDefault();
@@ -40,18 +34,6 @@
             event.preventDefault();
             document.querySelector('input[type="checkbox"][name="offtopic"][value="1"]').click();
         };
-
-        // insert link
-        if (event.key === 'Enter') {
-            event.preventDefault();
-            var modalDiv = document.querySelector('div.rtif-link-modal.show');
-
-            if (modalDiv) {
-                modalDiv.querySelector('button.btn.btn-secondary').click();
-            } else {
-                console.log("Visible div with a class containing 'rtif-link-modal' not found");
-            }
-        }
 
         // send comment
         if ((event.ctrlKey || event.metaKey) && event.key === 'Enter') {
